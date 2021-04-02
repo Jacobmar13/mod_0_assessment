a47bba0592191c65e90cef0b36c0db3096b48b0c
Jacob Martinez <jacobmarcareer@gmail.com>
2021-04-02 14:23:26 -0600

    Complete DEVELOPER challenge

---
+++ models/model_b.rb
@@ -0,0 +1,11 @@
+# DEVELOPER
+developer_self = {
+  "first_name" => "Jacob",
+  "last_name" => "Martinez",
+  "age" => 26,
+  "height_inches" => 68.5,
+  "engaged" => true,
+  "eager_for_school" => true,
+  "mod_0_be_cohorts" => ["Jacob Piland", "Caroline Tan", "Brian Fletcher"],
+  "mod_0_fe_cohorts" => ["Nina Brissey", "Megan McBride", "Wil Huffman"]
+}
