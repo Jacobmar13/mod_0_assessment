149c48001000c49691eabd246a1748c9b6ff5930
Jacob Martinez <jacobmarcareer@gmail.com>
2021-04-02 14:15:34 -0600

    Complete DINOSAUR challenge

---
+++ models/model_a.rb
@@ -0,0 +1,11 @@
+# DINOSAUR
+dinosaur_brontosaurus = {
+  "family" => "Diplodocidae",
+  "time_period" => "Late Jurassic",
+  "length_feet" => 72,
+  "weight_ton" => 15,
+  "herbivore" => true,
+  "extinct" => true,
+  "related_dinosaurs" => ["Apatosaurus", "Supersaurus", "Barosaurus"],
+  "enemy_dinosaurs" => ["Allosaurus", "Epanterias", "Torvosaurus"]
+}
