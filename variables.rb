dc6d91cf9681b0780d59ef261ba30a93ac966bc4
Jacob Martinez <jacobmarcareer@gmail.com>
2021-04-02 14:01:34 -0600

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,9 @@
+movie = "Blade Runner"
+is_entertaining = true
+movie = "Patlabor the Movie"
+num_4 = 4
+num_6 = 6
+product = num_4 * num_6
+favorite_foods = ["Chicken wings", "Steak", "Fettucine Alfredo"]
+favorite_foods.slice!(0)
+print movie.length
